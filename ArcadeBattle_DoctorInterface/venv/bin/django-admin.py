6867c74bc4fb17b9fa921a1f5aed7659b132dc6a
Rafael Direito <rafael.neves.direito@ua.pt>
2019-04-01 13:37:41 +0100

    Mail sending

--- ArcadeBattle_DoctorInterface/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rd/Desktop/UA/UA_6_Semestre/PI/ArcadeBattle_DoctorInterface/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
